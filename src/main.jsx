910decccd108d61699f6a48f5f208bd19750beef
towlem-cloud <towlem@ensworth.com>
2026-07-21 08:21:00 -0500

    Install coach identity bridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { installCoachIdentityBridge } from "./installCoachIdentityBridge.js";
+
+installCoachIdentityBridge();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
