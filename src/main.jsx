a58f52f7858434a25412dd463b49515091e39cdb
towlem-cloud <towlem@ensworth.com>
2026-07-21 08:29:37 -0500

    Install CSV export safety guard

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { installCoachIdentityBridge } from "./installCoachIdentityBridge.js";
+import { installCsvSafetyBridge } from "./installCsvSafetyBridge.js";
 
 installCoachIdentityBridge();
+installCsvSafetyBridge();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
